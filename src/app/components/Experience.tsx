69bf0b6221f2f6100e91627f2e9d4a8cc6b2c4ca
bradenhuang <bradenhuang01@gmail.com>
2024-10-19 03:40:36 -0600

    Small change to work experience listing

--- src/app/components/Experience.tsx
+++ src/app/components/Experience.tsx
@@ -7,19 +7,31 @@ const Experience = () => {
         <div className='mb-8 text-center text-3xl md:text-5xl font-bold'>
           Experience
         </div>
-        <div className='bg-slate-800 p-6 rounded-lg shadow-lg'>
-          <div className='flex flex-col md:flex-row justify-between items-start'>
-          <div className='text-lg md:text-xl font-semibold'>
-              Software Developer · ECSSEN Career School
+        <a href = 'https://github.com/BCharity-Net/good'>
+          <div className='bg-slate-800 p-6 rounded-lg shadow-lg hover:bg-slate-700' >
+            <div className='flex flex-col md:flex-row justify-between items-start'>
+            <div className='text-lg md:text-xl font-semibold'>
+                Software Developer · ECSSEN Career School
+              </div>
+              <div className='text-sm md:text-base text-gray-400'>
+                June — August 2024
+              </div>
             </div>
-            <div className='text-sm md:text-base text-gray-400'>
-              June — August 2024
+            <div className='mt-4 text-base md:text-lg'>
+              Worked on the developement of the BCharity website, a decentralized platform built with Lens Protocol to help charities maintain clarity in their operations.
+            </div>
+            <div className='mt-4 text-base md:text-lg'>
+              <div className='font-bold'>Key Contributions:</div>
+              <ul className='list-disc list-inside'>
+                <li>Developed and designed components for the BCharity website to handle user request posts</li>
+                <li>Successfully migrated the application from deprecated Lens Protocol V1 to Lens V2</li>
+                <li>Identified and fixed bugs to enhance user experience</li>
+              </ul>
+              <div className='mt-4 text-base md:text-lg'>
+              </div>
             </div>
           </div>
-          <div className='mt-4 text-base md:text-lg'>
-            Developed and designed components for the BCharity website to handle user request posts. Worked on migration from deprecated libraries and resolved user bugs.
-          </div>
-        </div>
+        </a>
       </div>
     </section>
   )
